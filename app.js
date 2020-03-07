4ef42e60911437713804ecec22e4a604724d4dca
Vedenjuoksu Piia <piia.vedenjuoksu@epec.fi>
2020-03-07 13:25:13 +0200

    update

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
-const axios = require ('axios');
+const axios = require('axios');
 
-axios.get('_osoite_robotin_serveriin_')
-    .then((res) => {
-        console.log(res);
-    });
 
-const regexp = 'regexpkomentotahan';
-    let joints = [];
-    let matches = res.data.matchAll(regexp);
-    let count = 0;
-    for  (const match of matches) {
-        count++;
-        if (count > 6) break;
-        const value = parseFloat(match[1]);
-        joints.push(value)
-    }
 
-    const main_loop = () => {
-        setTimeout(()=> {
-            //koodi
-            const time_stamp = new Date();
-            const delta = time_stamp - start_time_stamp;
-            main_loop();
-        },10);
-    }
-    main_loop();
+const main_loop = () => {
+    setTimeout(() => {
+        //koodi
+        let start_time_stamp = new Date();
+        axios.get('https://fanuc-robot-http-server.herokuapp.com/')
+            .then((res) => {
+                const time_stamp = new Date();
+                const delta = time_stamp - start_time_stamp;
+                const regexp = 'Joint   [1-6]: *(-?.*)'; //'Joint +[0-9]: +(-*[0-9]+\.[0-9]+)';
+                let joints = [];
+                let matches = res.data.matchAll(regexp);
+                let count = 0;
+                for (const match of matches) {
+                    count++;
+                    if (count > 6) break;
+                    const value = parseFloat(match[1]);
+                    joints.push(value)
+                }                
+                console.log(start_time_stamp, joints, delta, 'ms');
+            });
+
+        main_loop();
+    }, 10);
+}
+main_loop();
